9c5db8e7f068d24dc2816b77ef6f563d93525bf2
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-05-11 00:04:13 +0300

    Update bruteforce.py

--- source_code/bruteforce.py
+++ source_code/bruteforce.py
@@ -1,26 +1,32 @@
-from bs4 import BeautifulSoup
-url = 'https://cir.di.ionio.gr/undergrad/AppliedPython/ethicalHacking/index_1.html'
-page = requests.get(url)
-soup = BeautifulSoup(page.content)
-print(soup)
-
-# Bruteforce to find password
 import requests
 from selenium import webdriver
 import string
 import random
 from bs4 import BeautifulSoup
 import time
+from bs4 import BeautifulSoup
 
 url = 'https://cir.di.ionio.gr/undergrad/AppliedPython/ethicalHacking/index_1.html'
 page = requests.get(url)
+soup = BeautifulSoup(page.content)
+print(soup)
+
+# Bruteforce to find password
+
+url = 'https://cir.di.ionio.gr/undergrad/AppliedPython/ethicalHacking/index_1.html'
+page = requests.get(url)
+
 # Create a BeautifulSoup object from the response
-driver = webdriver.Firefox('geckodriver/')
+
+driver = webdriver.Firefox()
+# driver = webdriver.Firefox('geckodriver/')
+
 all_ascii = string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation
 
 for i in range(3):
     time.sleep(20)
     driver.get(url)
+    
     # Create a random string
     bruteforce_pass = ran = ''.join(random.choices(all_ascii, k = 10))
     print("Testing password ", bruteforce_pass)
